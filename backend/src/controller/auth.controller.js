e18846d8c60fbf836c0ff5b084e63776ee20c57f
namankrbhagat <namankumarbhagat.4226@gmail.com>
2025-09-30 01:13:45 +0530

    more bugs

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -43,6 +43,8 @@ export const signup = async (req,res) => {
       res.status(400).json({message: "Invalid user data"})
     }
   }catch (error){ 
+    console.error("Error in signup controller", error.message)
+    res.status(500).json({message:"Internal Server Error"})
   }
 }
 
